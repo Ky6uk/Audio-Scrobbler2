1316190dbfaeb3c0863a076f9b2b3c562f97e265
Roman (Ky6uk) Nuritdinov <ky6uk.kun@gmail.com>
2012-07-09 16:53:14 +0600

    0.05 prepare

--- Audio/Scrobbler2/lib/Audio/Scrobbler2.pm
+++ Audio/Scrobbler2/lib/Audio/Scrobbler2.pm
@@ -142,9 +142,14 @@ Audio::Scrobbler2 - Interface to last.fm scrobbler API
 
     use Audio::Scrobbler2;
 
-    my $scrobbler = Audio::Scrobbler2->new;
+    my $scrobbler = Audio::Scrobbler2->new($api_key, $api_secret);
+    my $api_token = $scrobbler->auth_getToken();
 
-    $scrobbler->scrobble("Artist", "Track");
+    # web-auth required
+    # http://www.last.fm/api/auth/?api_key=$api_key&token=$api_token
+    my $api_session = $scrobbler->auth_getSession();
+
+    $scrobbler->track_scrobble("Artist Name", "Track Name");
 
 
 =head1 METHODS
